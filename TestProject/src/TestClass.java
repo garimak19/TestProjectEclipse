6c69b82a1bad8005bfe3383189ed05496bb52a6a
garimak19 <lovelygarima@gmail.com>
2021-02-03 16:24:12 -0800

    Edit two

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("eclise git testing");
+		System.out.println("eclipse git testing");
 	System.out.println("testing pull");
 	}
 
