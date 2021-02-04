d51f5592bbe6cfade8b458024e0ed032b4c08261
garimak19 <59993443+garimak19@users.noreply.github.com>
2021-02-03 16:23:04 -0800

    Update TestClass.java

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("eclise git testing");
-
+	System.out.println("testing pull");
 	}
 
 }
